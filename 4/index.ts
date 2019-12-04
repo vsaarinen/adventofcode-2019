460f3b2ba843b3b7f08517742a026ea91715b2b9
Ville Saarinen <ville.saarinen@gmail.com>
2019-12-04 14:20:26 +0200

    Add solutions for day 4

---
+++ 4/index.ts
@@ -0,0 +1,23 @@
+import * as R from 'ramda'
+
+const checkHasExactlyTwoAdjacentNumbers = R.pipe(
+  // Split on character change. A bit buggy, generates extra 1-length arrays,
+  // but doesn't matter in this case
+  R.split(/(?<=([1-9]))(?=[1-9])(?!\1)/),
+  R.any(R.propEq('length', 2)),
+)
+const checkAscendingNumbers = R.pipe<string, string[], string[][], boolean>(
+  R.split(''),
+  R.aperture(2),
+  R.all(R.apply(R.lte as any)),
+)
+
+const countOfNumbers = R.pipe(
+  R.map(R.toString),
+  R.filter(
+    R.allPass([checkHasExactlyTwoAdjacentNumbers, checkAscendingNumbers]),
+  ),
+  R.length,
+)
+const range = R.range(172930, 683082 + 1)
+console.log('Count of matching numbers:', countOfNumbers(range))
