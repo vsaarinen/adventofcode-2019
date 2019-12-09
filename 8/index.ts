8cce0d1a7e379882632173d8253e099d97dd4aca
Ville Saarinen <ville.saarinen@gmail.com>
2019-12-09 10:25:07 +0200

    Add 8b

--- 8/index.ts
+++ 8/index.ts
@@ -9,35 +9,22 @@ const parseInput = R.pipe(
   R.map((c: string) => parseInt(c, 10)),
 )
 
-const generateLayers = R.pipe<number[], number[][], number[][][]>(
-  R.splitEvery(ROW_WIDTH),
-  R.splitEvery(ROW_COUNT),
-)
+const generateLayers = R.splitEvery(ROW_WIDTH * ROW_COUNT)
+const makePixelArray = R.transpose
+const resolvePixel = R.find(R.complement(R.equals(2)))
 
-const countNumber = (n: number) =>
-  R.pipe<
-    number[][],
-    number[],
-    {
-      [index: string]: number
-    },
-    number
-  >(R.flatten, R.countBy(R.identity), R.prop(n.toString()))
-
-const layerWithFewestZeros = (layers: number[][][]) =>
-  R.reduce<number[][], number[][]>(
-    R.minBy(countNumber(0)),
-    layers[0],
-  )(layers.slice(1))
-const getChecksum: (xs: number[][]) => number = R.converge(R.multiply, [
-  countNumber(1),
-  countNumber(2),
-])
-
-const checksum = R.pipe(
+const finalImage = R.pipe<string, number[], number[][], number[][], number[]>(
   parseInput,
   generateLayers,
-  layerWithFewestZeros,
-  getChecksum,
+  makePixelArray,
+  R.map(resolvePixel),
 )(readFileSync('8/input.txt').toString())
-console.log('checksum:', checksum)
+
+const printImage = R.pipe(
+  R.map(R.ifElse(R.equals(0), R.always(' '), R.always('X'))),
+  R.splitEvery(ROW_WIDTH),
+  R.map(R.join('')),
+  R.join('\n'),
+)
+console.log('final image:')
+console.log(printImage(finalImage))
